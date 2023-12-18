5aa2ae982a8c7750794dcd79af8098ab5ef26028
AlvarooRM <yomismoarm@hotmail.com>
2023-12-18 11:01:43 +0100

    Examen DIW Alvaro

---
+++ static/app.js
@@ -0,0 +1,18 @@
+console.log("Hola mundo...");
+
+function abrirBorrar() {
+    document.getElementById('borrar').style.display = 'flex';
+}
+
+function cerrarBorrar() {
+    document.getElementById('borrar').style.display = 'none';
+
+}
+
+function nuevoCoche() {
+    document.getElementById('nuevoCoche').style.display = 'flex';
+}
+
+function cerrarCoche() {
+    document.getElementById('nuevoCoche').style.display = 'none';
+}
